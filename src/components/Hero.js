97756e226bdd387d5cf45fc008e44cc6a32b5d7b
benbenz101 <bensonbenzene@gmail.com>
2022-02-23 15:43:33 +0100

    reworked navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,14 @@
 
 
+function Hero ()={
+    return (
+    <>
+    
+    
+    </>
+    )
+}
+
 
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            Rent Movers
           </a>
           <button
             className="navbar-toggler"
@@ -18,7 +18,7 @@ function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
                   Home
@@ -86,13 +86,11 @@ function Navbar() {
                   Disabled
                 </a>
               </li> */}
-            </ul>
-            <form className="d-flex">
-              {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"> */}
+
               <button className="btn btn-outline-success" type="submit">
                 LOG IN
               </button>
-            </form>
+            </ul>
           </div>
         </div>
       </nav>

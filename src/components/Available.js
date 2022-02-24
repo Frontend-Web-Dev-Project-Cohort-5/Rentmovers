9a7642b27f7af8e9273bb302da9be15d37f1c799
benbenz101 <bensonbenzene@gmail.com>
2022-02-24 12:57:53 +0100

    started work on available component

---
+++ src/components/Available.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Available() {
+  return (
+    <>
+      <h3 className="text-center">We are available in many Locations in Lagos</h3>
+
+      <Availablecard />
+    </>
+  );
+}
+
+function Availablecard() {
+  return <></>;
+}
+
+export default Available;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 //components
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Available from "../components/Available";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero/>
+      <Available/>
     </>
   );
 }

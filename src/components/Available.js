846a5c9c90a99046256f3febdfd4eaa8fa5a7384
benbenz101 <bensonbenzene@gmail.com>
2022-02-24 13:39:32 +0100

    working on available component

--- src/components/Available.js
+++ src/components/Available.js
@@ -1,17 +1,36 @@
 import React from "react";
+import Ikoyirm from "../media/Ikoyirm.png";
 
 function Available() {
   return (
     <>
-      <h3 className="text-center">We are available in many Locations in Lagos</h3>
+      <div className="mt-3">
+        <h3 className="text-center">
+          We are available in many Locations in Lagos
+        </h3>
+        <div className="row">
+          <div className="col">
+            <img src={Ikoyirm} alt="IKOYI" />
+          </div>
+          <div className="col">
+            <img src="" alt="VI" />
+          </div>
+          <div className="col">
+            <img src="" alt="LEKKI" />
+          </div>
+          <div className="col">
+            <img src="" alt="AJAH" />
+          </div>
+        </div>
+      </div>
 
-      <Availablecard />
+      {/* <Availablecard /> */}
     </>
   );
 }
 
-function Availablecard() {
-  return <></>;
-}
+// function Availablecard() {
+//   return <></>;
+// }
 
 export default Available;

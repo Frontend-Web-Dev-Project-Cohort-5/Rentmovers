75f255c4b0aad1e06d5c0fc777db8ca9a307dc9d
Raphael Ovonlen <princely233@gmail.com>
2022-02-24 14:50:06 +0100

    Corrected Virm.png to virm.png

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,7 @@ let Hero = () => {
     <div className="row hero11 bg-secondary">
       <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
         <div className="row mt-4 ms-4">
-          <h1 className="text-light">
-            EASY WAY TO FIND A PERFECT PROPERTY
-          </h1>
+          <h1 className="text-light">EASY WAY TO FIND A PERFECT PROPERTY</h1>
 
           <p className="text-light">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo,

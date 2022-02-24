ed2b5d1b91669ba8cfb139e32aae7465746e75a1
benbenz101 <bensonbenzene@gmail.com>
2022-02-24 12:43:40 +0100

    modified content in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,41 +1,20 @@
-
 let Hero = () => {
-    return(
-        <div className="row hero11 bg-secondary">
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+  return (
+    <div className="row hero11 bg-secondary">
+      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        <div className="row mt-4 ms-4">
+          <h1 className="text-secondary">
+            EASY WAY TO FIND A PERFECT PROPERTY
+          </h1>
 
-                <div className="row mt-4 ms-4">
-                    <h1 className="text-secondary">HELLO, I AM BENSON CJ</h1>
-                    
-                    <p className="text-light">I PIECE YOUR ROUGH IDEAS INTO BEAUTIFUL AND INTERACTIVE MASTERPIECE THE WORLD WILL SEE AND APPRECIATE</p> 
-                </div>
-            </div>
+          <p className="text-light">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo,
+            ratione?
+          </p>
         </div>
-    )
-}
-
-
-
-// function Hero() {
-//     return (
-//     <>
-//     <div className=" row hero11 bg-secondary">
-//         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-
-//             <div className="row mt-4 ms-4">
-//               <h1 className="text-secondary">HELLO, I AM BENSON CJ</h1>
-              
-//               <p className="text-light">I PIECE YOUR ROUGH IDEAS INTO BEAUTIFUL AND INTERACTIVE MASTERPIECE THE WORLD WILL SEE AND APPRECIATE</p> 
-//         </div>
-//     </div>
-
-//     </>
-//     );
-// }
-
-
-
-
-
+      </div>
+    </div>
+  );
+};
 
 export default Hero;

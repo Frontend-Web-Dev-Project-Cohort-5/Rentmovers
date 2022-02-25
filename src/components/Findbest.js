45e2262841659460a157f4113370571ae47fcb0e
benbenz101 <bensonbenzene@gmail.com>
2022-02-25 09:37:18 +0100

    working on find best, added image and writeup

---
+++ src/components/Findbest.js
@@ -0,0 +1,23 @@
+// import React from "react";
+import findbest from "../media/findbest.jpg";
+
+function Findbest() {
+  return (
+    <>
+      <div className="card text-dark">
+        <img src={findbest} className="card-img" alt="find best image" />
+        <div className="card-img-overlay">
+          <h5 className="card-title">Find the best Real Estate</h5>
+          <p className="card-text">
+            We provide a complete service for the rental of real estate.
+          </p>
+          <button className="btn btn-outline-success" type="submit">
+            FIND US
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Findbest;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,16 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Available from "../components/Available";
+import Findbest from "../components/Findbest";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Navbar />
-      <Hero/>
-      <Available/>
+      <Hero />
+      <Available />
+      <Findbest />
       
     </>
   );

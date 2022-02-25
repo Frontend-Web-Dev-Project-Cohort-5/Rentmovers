85bc9d69072495d470db00cc276414f607553631
benbenz101 <bensonbenzene@gmail.com>
2022-02-25 10:18:30 +0100

    initialised pages

--- src/components/Findbest.js
+++ src/components/Findbest.js
@@ -4,8 +4,12 @@ import findbest from "../media/findbest.jpg";
 function Findbest() {
   return (
     <>
-      <div className="card text-dark">
-        <img src={findbest} className="card-img" alt="find best image" />
+      <div className="card text-dark mt-4 mb-4 mx-5">
+        <img
+          src={findbest}
+          className="card-img rounded"
+          alt="find best image"
+        />
         <div className="card-img-overlay">
           <h5 className="card-title">Find the best Real Estate</h5>
           <p className="card-text">
--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,6 +1,13 @@
+import React from 'react'
+
+function Error404() {
+  return (
+    <div>Error404</div>
+  )
+}
+
+export default Error404
 
 
 
 
-
-export default Error404;

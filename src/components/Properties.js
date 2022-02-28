270b3a814b3b80a668df37bdd51dc456977c5707
Adebanjo Ebenezer Adeniji <aeben.adebanjo@gmail.com>
2022-02-28 13:53:25 +0100

    properties page little change

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,35 +1,42 @@
-import Rmlogo1 from "../media/Rmlogo1.png";
 function Properties() {
   return (
     <>
-      <div className="container">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body col-md-6 col-sm-6">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
+      <div className="card mb-3" style={{ maxWidth: "540px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="..." className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
             </div>
           </div>
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body col-sm-6 col-md-6">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
+        </div>
+      </div>
+      <div className="card mb-3" style={{ maxWidth: "540px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="..." className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
             </div>
           </div>
         </div>

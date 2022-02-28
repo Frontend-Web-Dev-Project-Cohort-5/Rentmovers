e950f1f6ca3b4c141eb99de20a72e1b5773ec68c
Raphael Ovonlen <princely233@gmail.com>
2022-02-28 01:56:24 +0100

    completed footer 96%

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,88 +1,77 @@
-import React from "react";
-import "../sass/App.css";
+import React from 'react';
+import '../sass/App.css';
+import Logo from '../media/Rmlogo1.png';
 
 const Footer = () => {
   return (
-    
-    <footer id="FooterComponent"
-    className="row justify-content-center">
-      <div>
-        <div className="row align-items-start">          
-        <div className="col">
-          <img src="../logo.svg" alt="logo" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, id
-            incidunt. Iusto suscipit aliquid eos, voluptates et ut blanditiis
-            nesciunt, expedita esse, eius minus perferendis corporis nam.
-            Necessitatibus, exercitationem aspernatur.
+    <footer id="FooterMain" className="row justify-content-center">
+      <div id="FooterSubMain">
+        <div className="col-3" id="FooterCol1">
+          <img src={Logo} alt="RentMovers" />
+          <p className="pFT">
+            RentMovers is a web-app that connects homeowners to their
+            prospective tenants and vice-versa without the hassle of going
+            through agents, and moving from house to house before making their
+            final decision.
           </p>
+          <div id="FooterIcons">
+            <i className="bi bi-facebook" href="https://facebook.com"></i>
+            <i className="bi bi-twitter" href="https://twitter.com"></i>
+            <i className="bi bi-instagram" href="https://instagram.com"></i>
+            <i className="bi bi-linkedin" href="https://linkedin.com"></i>
+          </div>
+          <p> &copy; 2022. All rights reserved</p>
         </div>
-        <table className="col">
-          
-            <thead>
+        <table className="col-6" id="FooterCol2">
+          <thead>
             <tr>
-            <th>
-              header1
-            </th>
-            <th>
-              header2
-            </th>
+              <th>Take a tour</th>
+              <th>Our Company</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Features</td>
+              <td>About Us</td>
+            </tr>
+            <tr>
+              <td>Partners</td>
+              <td>Agents</td>
+            </tr>
+            <tr>
+              <td>Support</td>
+              <td>Contact Us</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="col-3" id="FooterCol3">
+          <table>
+            <thead>
+              <tr>
+                <th>Subscribe</th>
               </tr>
             </thead>
 
-
             <tbody>
               <tr>
-                <td>
-                  Hi
+                <td className="pFT">
+                  Subscribe to get latest property blog news from us.
                 </td>
-                <td>
-                  Hello
-                </td>
-                </tr>
+              </tr>
               <tr>
                 <td>
-                  Hi2
+                  <form action="https://formspree.io/f/mpzbrwqv">
+                    Email Address
+                  </form>
                 </td>
-                <td>
-                  Hello2
-                </td>
-                </tr>
-              <tr>
-                <td>
-                  Hi3
-                </td>
-                <td>
-                  Hello3
-                </td>
-                </tr>
-              </tbody>
+              </tr>
+            </tbody>
           </table>
-      
-        <div className="col">
-            <table>
-              <thead>
-                <tr>
-                  <th>
-                    Signin
-                  </th>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr>
-                  <td>
-                    search
-                  </td>
-                </tr>
-              </tbody>
-         </table>
-            
         </div>
-        </div>
-        </div>
-        </footer>
-    
+      </div>
+    </footer>
   );
 };
 

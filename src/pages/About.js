06245fc33be81fdd0ea2ba737b24b37404cdc999
benbenz101 <bensonbenzene@gmail.com>
2022-03-02 17:52:53 +0100

    building the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,91 @@ import React from 'react'
 function About() {
   return (
     <>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, commodi?</p>
+      <h3>About RentMovers</h3>
+      <p>
+        RentMovers is a web-app that connects homeowners to their prospective
+        tenants and vice versa without the hassle of going through agents and
+        moving from house to house before making their final decision.
+      </p>
+      <div>
+        <p>
+          When you are looking for a rental property, you will find that there
+          are hundreds of housing companies and thousands of private landlords,
+          who are renting out properties. But where do you find all these
+          landlords and contacts? How do you know whether they have an available
+          rental which suits your needs? How do you stay updated whenever new
+          rentals come to the market? At RentMovers we collect almost all
+          available rental properties in one search. So it is easy for you to
+          find exactly what matches your criterias and get a new home faster. As
+          a result of that, we save you a lot of time in your housing search.
+          Easy and simple.
+        </p>
+      </div>
+      <h3>Meet The Team</h3>
+      <div className="row">
+        <div className="col">
+          <div>
+            <img src="" alt="benson-pic" />
+          </div>
+          <div>
+            <img src="" alt="github" />
+            <img src="" alt="linkedin" />
+            <img src="" alt="twitter" />
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+        </div>
+        <div className="col">
+          <div>
+            <img src="" alt="benson-pic" />
+          </div>
+          <div>
+            <img src="" alt="github" />
+            <img src="" alt="linkedin" />
+            <img src="" alt="twitter" />
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <div>
+            <img src="" alt="benson-pic" />
+          </div>
+          <div>
+            <img src="" alt="github" />
+            <img src="" alt="linkedin" />
+            <img src="" alt="twitter" />
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+        </div>
+
+        <div className="col">
+          <div>
+            <img src="" alt="benson-pic" />
+          </div>
+          <div>
+            <img src="" alt="github" />
+            <img src="" alt="linkedin" />
+            <img src="" alt="twitter" />
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+        </div>
+
+        <div className="col">
+          <div>
+            <img src="" alt="benson-pic" />
+          </div>
+          <div>
+            <img src="" alt="github" />
+            <img src="" alt="linkedin" />
+            <img src="" alt="twitter" />
+          </div>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default About
